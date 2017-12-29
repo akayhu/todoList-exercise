c2abaa0635b1e06c7747122dea6bebbd0efbfed6
akay.hu[胡凱綺] <akay.hu@104.com.tw>
2017-12-29 15:54:16 +0800

    update unit test

--- src/actions/__tests__/action.test.js
+++ src/actions/__tests__/action.test.js
@@ -1,47 +1,67 @@
 import * as actions from '../index';
 
-describe('addTodoAction', () => {
-  it('addTodoAction', () => {
-    const text = 'test word';
-    const responseDate = {
-      type: actions.ADD_TODO,
-      id: 0,
-      text
-    };
-    expect(actions.addTodo(text)).toEqual(responseDate)
-  }),
-  it('viewFilterAction', () => {
-    const filter = 'all';
-    const responseDate = {
-      type: actions.VIEW_FILTER,
-      filter
-    };
-    expect(actions.viewFilter(filter)).toEqual(responseDate)
-  }),
-  it('toggleTodoAction', () => {
-    const id = 0;
-    const responseDate = {
-      type: actions.TOGGLE_TODO,
-      id
-    };
-    expect(actions.toggleTodo(id)).toEqual(responseDate)
-  }),
-  it('deltetTodoAction', () => {
-    const id = 0;
-    const responseDate = {
-      type: actions.DELETE_TODO,
-      id
-    };
-    expect(actions.deltetTodo(id)).toEqual(responseDate)
-  }),
-  it('editTodoAction', () => {
-    const id = 0;
-    const text = 'edit test';
-    const responseDate = {
-      type: actions.EDIT_TODO,
-      id,
-      text
-    };
-    expect(actions.editTodo(id, text)).toEqual(responseDate)
-  })
-})
+describe('actions', () => {
+
+  beforeEach(() => {
+    jest.resetModules();
+  });
+
+  describe('type: ADD_TODO', () => {
+    it('ADD_TODO_action', () => {
+      const text = 'test word';
+      const responseDate = {
+        type: actions.ADD_TODO,
+        id: 0,
+        text
+      };
+      expect(actions.addTodo(text)).toEqual(responseDate)
+    });
+  });
+
+  describe('type: VIEW_FILTER', () => {
+    it('VIEW_FILTER_ction', () => {
+      const filter = 'all';
+      const responseDate = {
+        type: actions.VIEW_FILTER,
+        filter
+      };
+      expect(actions.viewFilter(filter)).toEqual(responseDate)
+    });
+  });
+
+  describe('type: TOGGLE_TODO', () => {
+    it('TOGGLE_TODO_action', () => {
+      const id = 0;
+      const responseDate = {
+        type: actions.TOGGLE_TODO,
+        id
+      };
+      expect(actions.toggleTodo(id)).toEqual(responseDate)
+    });
+  });
+
+  describe('type: DELETE_TODO', () => {
+    it('DELETE_TODO_action', () => {
+      const id = 0;
+      const responseDate = {
+        type: actions.DELETE_TODO,
+        id
+      };
+      expect(actions.deltetTodo(id)).toEqual(responseDate)
+    });
+  });
+  
+  describe('type: EDIT_TODO', () => {
+    it('EDIT_TODO_action', () => {
+      const id = 0;
+      const text = 'edit test';
+      const responseDate = {
+        type: actions.EDIT_TODO,
+        id,
+        text
+      };
+      expect(actions.editTodo(id, text)).toEqual(responseDate)
+    });
+  });
+  
+});
--- src/reducers/__tests__/filterReducer.test.js
+++ src/reducers/__tests__/filterReducer.test.js
@@ -1,24 +1,38 @@
 import filterReducer from '../filterReducer';
 
 describe('filterReducer', () => {
-  it('filterReducer all', () => {
-    const responseAllDate = 'all';
-    expect(filterReducer(undefined, {})).toEqual(responseAllDate)
-  }),
-  it('filterReducer completed', () => {
-    const completedAction = { 
-      type: 'VIEW_FILTER',
-      filter: 'completed'
-    };
-    const responseCompletedDate = 'completed';
-    expect(filterReducer(undefined, completedAction)).toEqual(responseCompletedDate)
-  }),
-  it('filterReducer unCompleted', () => {
-    const unCompletedAction = { 
-      type: 'VIEW_FILTER',
-      filter: 'unCompleted'
-    };
-    const responseUnCompletedDate = 'unCompleted';
-    expect(filterReducer(undefined, unCompletedAction)).toEqual(responseUnCompletedDate)
-  })
-})
+
+  beforeEach(() => {
+    jest.resetModules();
+  });
+
+  describe('filterReducer all', () => {
+    it('filterReducer all', () => {
+      const responseAllDate = 'all';
+      expect(filterReducer(undefined, {})).toEqual(responseAllDate)
+    });
+  });
+
+  describe('filterReducer completed', () => {
+    it('filterReducer completed', () => {
+      const completedAction = { 
+        type: 'VIEW_FILTER',
+        filter: 'completed'
+      };
+      const responseCompletedDate = 'completed';
+      expect(filterReducer(undefined, completedAction)).toEqual(responseCompletedDate)
+    });
+  });
+  
+  describe('filterReducer unCompleted', () => {
+    it('filterReducer unCompleted', () => {
+      const unCompletedAction = { 
+        type: 'VIEW_FILTER',
+        filter: 'unCompleted'
+      };
+      const responseUnCompletedDate = 'unCompleted';
+      expect(filterReducer(undefined, unCompletedAction)).toEqual(responseUnCompletedDate)
+    });
+  });
+  
+});
