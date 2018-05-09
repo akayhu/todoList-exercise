e1d7c4017b6bb5a9e6c738920fb728574c292046
akay.hu[胡凱綺] <akay.hu@104.com.tw>
2018-05-09 18:11:44 +0800

    create react router

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,9 @@
 import React, { Component, Fragment } from 'react';
 import './index.css';
 import Header from '../containers/Header/index';
+import Nav from '../containers/Nav/index';
 import Home from '../containers/Index/index';
-import { BrowserRouter as Router, Route, Link, NavLink } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import TodoListMain from '../containers/TodoList';
 
 class App extends Component {
@@ -12,12 +13,7 @@ class App extends Component {
 				<Header />
 				<Router>
 					<div className="main">
-						<nav>
-							<IsNavLink activeOnlyWhenExact={ true } to="/" label="首頁" />
-							<IsNavLink to="/todoList" label="TodoList 練習" />
-							<IsNavLink to="/topics" label="Topics 範例" />
-							<IsNavLink to="/profile/9527" label="取 Profile ID 範例" />
-						</nav>
+						<Nav />
 						<hr />
 						<Route exact path="/" component={ Home } />
 						<Route path="/todoList" component={ TodoListMain } />
@@ -29,22 +25,6 @@ class App extends Component {
 		);
 	}
 }
-
-const IsNavLink = ({ label, to, activeOnlyWhenExact }) => (
-	<Route
-		path={ to }
-		exact={ activeOnlyWhenExact }
-		children={ ({ match }) => {
-			const isSelect = match ? 'select' : '';
-			const isActive = match ? 'active' : '';
-			return (
-				<div className={ `${isActive} ` }>
-					<NavLink activeClassName={ isSelect } to={ to }>{ label }</NavLink>
-				</div>
-			);
-		 }}
-	/>
-);
 
 const Topics = ({ match }) => (
 	<div className="topics_main">
---
+++ src/containers/Nav/index.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import './index.css';
+import { Route, Link } from "react-router-dom";
+
+const navData = [
+	{
+		label: '首頁',
+		to: '/',
+		activeOnlyWhenExact: true
+	},
+	{
+		label: 'TodoList 練習',
+		to: '/todoList',
+		activeOnlyWhenExact: false
+	},
+	{
+		label: 'Topics 範例',
+		to: '/topics',
+		activeOnlyWhenExact: false
+	},
+	{
+		label: '取 Profile ID 範例',
+		to: '/profile/9527',
+		activeOnlyWhenExact: false
+	}
+];
+
+class NavLink extends Component {
+	render() {
+		const { to, activeOnlyWhenExact, label } = this.props.data;
+		return (
+			<Route
+				path={ to }
+				exact={ activeOnlyWhenExact }
+				children={ ({ match }) => {
+					const isSelect = match ? 'select' : '';
+					const isActive = match ? 'active' : '';
+					return (
+						<div className={ `${isActive} ` }>
+							<Link activeclassname={ isSelect } to={ to }>{ label }</Link>
+						</div>
+					);
+				} }
+			/>
+		);
+	}
+}
+
+class Nav extends Component {
+	render() {
+		return (
+			<nav className="nav">
+				{
+					navData.map((items, index) => <NavLink data={ items } key={ index } /> )
+				}
+			</nav>
+		)
+	}
+}
+
+export default Nav;
